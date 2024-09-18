98639bddc5988b218cc870cfc1bc16b9e44472f2
Mcl0vinnn <cpascua0608@bhncdsb.ca>
2024-09-18 15:36:53 +0000

    updated

--- Loops/lopps_4.py
+++ Loops/lopps_4.py
@@ -1,9 +1,6 @@
-
-
 name = input ('Please enter a name: ')
 
 while name:
     name = input ('Please enter a name: ')
     print (name ,'has been invited!')
-   
 print ('This is the total amount of people you invited: ', name)
